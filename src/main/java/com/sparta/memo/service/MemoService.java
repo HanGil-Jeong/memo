fc47584926a62efb5266d4fef7dbfc6f29559cbb
Hangil <jhg9240@gmail.com>
2026-04-08 14:10:31 +0900

    IoC Container 와 Bean

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,11 +4,9 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public class MemoService {
 
 	private final MemoRepository memoRepository;
