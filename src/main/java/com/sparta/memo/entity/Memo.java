a5a67a8e45b923bf1c094946d0da1471529c36ff
Hangil <jhg9240@gmail.com>
2026-06-14 10:13:49 +0900

    feat: Creat Memo Entity

---
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -0,0 +1,18 @@
+package com.sparta.memo.entity;
+
+import com.sparta.memo.dto.RequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Memo {
+	private Long id;
+	private String title;
+	private String contents;
+
+	public void update(RequestDto requestDto) {
+		this.title = requestDto.getTitle();
+		this.contents = requestDto.getContents();
+	}
+}
